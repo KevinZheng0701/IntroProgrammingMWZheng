cdbaba64a6ae2d0d1e47e9ba27d0371545600ec8
Kevin Zheng <kevinzheng700@gmail.com>
2024-02-14 18:05:31 -0500

    Added bounce and reset to ball
    Ball bounce off ball and reset when hitting the left or right ball.

--- PongKevin/Assets/Scripts/BallController.cs
+++ PongKevin/Assets/Scripts/BallController.cs
@@ -9,34 +9,45 @@ public class BallController : MonoBehaviour
     public float force = 200f;
     private float xDirection;
     private float yDirection;
+    public bool inPlay;
+    public Vector3 ballStartPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        xDirection = Random.Range(0, 2);
-        yDirection = Random.Range(0, 2);
-        Vector3 direction = new Vector3(0, 0, 0);
-        if (xDirection == 0)
-        {
-            direction.x = -1;
-        } else if (xDirection == 1) {
-            direction.x = 1;
-        }
-        if (yDirection == 0)
-        {
-            direction.y = -1;
-        }
-        else if (yDirection == 1)
-        {
-            direction.y = 1;
-        }
-        // add force to start movement
-        rbBall.AddForce(direction * force);
+        Launch();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (inPlay == false)
+        {
+            transform.position = ballStartPosition;
+            Launch();
+        }
+    }
+    // Start the game and push the ball around randomly
+    void Launch()
+    {
+        xDirection = Random.Range(0, 2);
+        yDirection = Random.Range(0, 2);
+        Vector3 direction = new Vector3(0, 0, 0);
+        direction.x = (xDirection == 0) ? -1 : 1;
+        direction.y = (yDirection == 0) ? -1 : 1;
+        // add force to start movement
+        rbBall.AddForce(direction * force);
+        inPlay = true;
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "LeftWall" || collision.gameObject.name == "RightWall")
+        {
+            rbBall.velocity = Vector3.zero;
+            inPlay = false;
+        } 
 
     }
+
 }
