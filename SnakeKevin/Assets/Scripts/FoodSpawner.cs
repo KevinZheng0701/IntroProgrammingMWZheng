19f88dd7e08b01d1876b2bd2b85f1878265d6e0e
Kevin Zheng <kevinzheng700@gmail.com>
2024-02-26 18:10:24 -0500

    Added food spawner
    Added food spawning to the game. Also added walls in the game.

---
+++ SnakeKevin/Assets/Scripts/FoodSpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour
+{
+    // Global variables
+    public GameObject foodPrefab;
+    public Transform wallTop;
+    public Transform wallBottom;
+    public Transform wallLeft;
+    public Transform wallRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnFood", 3, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Spawn food randomly
+    void SpawnFood()
+    {
+        // get random spawn location
+        int xPos = (int)Random.Range(wallLeft.position.x + 1, wallRight.position.x - 1);
+        int yPos = (int)Random.Range(wallBottom.position.y + 1, wallTop.position.y - 1);
+        Instantiate(foodPrefab, new Vector3(xPos, yPos, 0), Quaternion.identity);
+    }
+}
